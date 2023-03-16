66b76f9a136ba94d5a1908ea73b691a13286e148
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-16 16:07:15 -0700

    Sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i,j;
+	long k;
+	float f;
+	
+	printf("Size of a char:%lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int:%i.\n", (unsigned int)sizeof(i));
+	printf("Size of a long int:%lu.\n", (unsigned long)sizeof(j));
+	printf("Size of a long long int:%i.\n", (signed long)sizeof(k));
+	printf("Size of a float:%f.\n", (unsigned long)sizeof(f));
+
+	return (0);
+}

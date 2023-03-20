47b2d452aa53fa2909096431ddef7c981fb064ba
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-20 14:36:17 -0700

    Prints the lower case alphabet except for e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[26], alpha;
+	int i = 0;
+
+	alpha = 'a';
+	while (i < 26)
+	{
+		if ((i!=4)&&(i!=16))
+		{
+			str[i] = alpha++;
+			putchar(str[i]);
+			i++;
+		}
+		else
+		{
+			i++;
+			alpha++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

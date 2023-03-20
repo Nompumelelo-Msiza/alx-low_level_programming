8f3160f4ef3e7b40900f420dabe22f0ab7e3644e
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-20 08:23:05 -0700

    Conditional statements that checks for positve,zero or negative values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n");
+		printf("%ld is positive\n");
 	else if (n == 0)
-		printf("%d is zero\n");
+		printf("%ld is zero\n");
 	else if (n < 0)
-		printf("%d is negative\n");
+		printf("%ld is negative\n");
 	return (0);
 }

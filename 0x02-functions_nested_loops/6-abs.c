26beeea5dbcb805d33ad5af194006aca7e5b2cb3
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 16:36:10 -0700

    Commutes the absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of int
+ * @n: value being checked
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
+
+

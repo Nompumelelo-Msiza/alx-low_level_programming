d5dfe32ca125539bef43d946457b63c266f26537
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 14:05:43 -0700

    Prints the lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- prints lowercase alphabets x10
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	int i=0;
+
+	alpha = 'a';
+	while (i <= 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

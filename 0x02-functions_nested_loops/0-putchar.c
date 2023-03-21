c6d221be64ef75c896f3447abf271af0b63bd489
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 11:55:31 -0700

    Prints a string _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

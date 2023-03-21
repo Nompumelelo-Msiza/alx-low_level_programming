b779aa33a3d566abf78c5de755b00fee200de1b8
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 15:21:06 -0700

    checks for alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,15 @@
 
 int _isalpha(int c)
 {
-	if (((c <= 'a') && (c >= 'b')) || ((c <= 'A') && (c >= 'Z')))
+	char low_a, low_z, upper_a, upper_z;
+
+	low_a = 'a';
+	low_z = 'z';
+	upper_a = 'A';
+	upper_Z = 'Z';
+	if ((c <= upper_a) && (c >= upper_z))
+	       	return (1);
+	else if ((c <= low_a) && (c >= low_z))
 		return (1);
 	else
 		return (0);

907bd74936a302da105ca62be4debdbc5ca8db2f
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 18:40:42 -0700

    prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,14 +14,16 @@ void times_table(void)
 		{
 			if ((j * i) < 10)
 			{
-				_putchar(j * i);
+				_putchar((j * i) + '0');
 				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(((j * i) / 10) + '0');
 				_putchar(((j * i) % 10) + '0');
 				_putchar(',');
+				_putchar(' ');
 			}
 			j++;
 		}

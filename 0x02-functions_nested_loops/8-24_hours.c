3b4f19f34f15f907f9f4e2911049a79762e8ed24
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-21 17:37:01 -0700

    Prints every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,12 +7,12 @@ void jack_bauer(void)
 {
 	int i = 0, j;
 	
-	while(i < 24)
+	while (i < 24)
 	{
 		j = 0;
-		while (j <60)
+		while (j < 60)
 		{
-			_putchar((i/10) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');

4905f368c80dcfe36d72e927da10d59602a17fa0
Nompumelelo Msiza <msizanompumelelo.m.99@gmail.com>
2023-03-23 03:28:18 -0700

    Prints numbers from 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int c;
+	
+	c = 48;
+	while (c < 58)
+	{
+		if(c != 50 && c != 52)
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+}
